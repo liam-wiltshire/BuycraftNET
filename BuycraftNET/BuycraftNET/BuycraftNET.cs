547bf1a99df9befc8044aa95b02be9a9ac773b3e
Liam Wiltshire <liam@w.iltshi.re>
2017-07-04 14:15:15 +0100

    Basic boilerplate

--- BuycraftNET/BuycraftNET/BuycraftNET.cs
+++ BuycraftNET/BuycraftNET/BuycraftNET.cs
@@ -8,6 +8,7 @@ using MiNET.Plugins.Attributes;
 using MiNET.Utils;
 
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
@@ -27,6 +28,12 @@ namespace BuycraftNET
             Context.PluginManager.LoadCommands(new Buycraft(this));
 //            Context.PluginManager.LoadCommands(new Buy(this));            
             EIO();
+
+            var commandChecker = new CommandChecker(this);
+            var autoEvent = new AutoResetEvent(false);
+
+            var stateTimer = new Timer(commandChecker.CheckCommands,
+                                   autoEvent, 10000, 300000);
         }
 
         public string GetDataFolder() => Path.Combine(new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)).LocalPath, "BuycraftNET");
@@ -51,6 +58,21 @@ namespace BuycraftNET
                 FileStream fs = temp.Create();
                 fs.Close();
             }
+        }
+    }
+
+    public class CommandChecker
+    {
+        private BuycraftNET Plugin;
+
+        public CommandChecker (BuycraftNET plugin)
+        {
+            Plugin = plugin;
+        }
+
+        public void function CheckCommands()
+        {
+            Console.WriteLine("Do Command Check!");
         }
     }
 }
