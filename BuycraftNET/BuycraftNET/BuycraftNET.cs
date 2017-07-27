dd2f10f3695c5604d2cb7911358d1ce28f4fa570
Liam Wiltshire <liam@w.iltshi.re>
2017-07-27 17:07:49 +0100

    Trying to get things working

--- BuycraftNET/BuycraftNET/BuycraftNET.cs
+++ BuycraftNET/BuycraftNET/BuycraftNET.cs
@@ -1,46 +1,59 @@
 using BuycraftNET.Command;
-//using BuycraftNET.Command.Buycraft;
-//using BuycraftNET.Command.Buy;
 
-using MiNET;
 using MiNET.Plugins;
 using MiNET.Plugins.Attributes;
-using MiNET.Utils;
+using MiNET.Worlds;
+using System.Collections.Generic;
 
 using System;
-using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
+using System.Timers;
 using System.IO;
-using System.Net.Http;
 using System.Reflection;
 
 using IniParser;
 using IniParser.Model;
+
+using BuycraftNET.Api;
+using MiNET.Plugins.Commands;
 
 namespace BuycraftNET
 {
     [Plugin(PluginName = "BuycraftNET", Description = "MiNET Buycraft plugin.", PluginVersion = "1.0", Author = "Buycraft")]
     public class BuycraftNET : Plugin
     {
-        private FileIniDataParser _parser;
+        private FileIniDataParser _parser = new FileIniDataParser();
         private IniData _config;
-        private static readonly HttpClient _httpClient = new HttpClient();
+        private ApiClient _apiClient;
         
         protected override void OnEnable()
         {
+            LogInfo("Buycraft has started");
 
-            this._parser = new FileIniDataParser();
+            //@TODO - This Is only in so we have commands to test with - needs removing at some point.
+            Context.PluginManager.LoadCommands(new VanillaCommands(Context.Server.PluginManager));
+            
             this._config = this._parser.ReadFile(this.GetConfigFile());
             
+            _apiClient = new ApiClient(this);
+            
             Context.PluginManager.LoadCommands(new Buycraft(this));
-//            Context.PluginManager.LoadCommands(new Buy(this));            
+            Context.PluginManager.LoadCommands(new Buy(this));            
 
             var commandChecker = new CommandChecker(this);
-            var autoEvent = new AutoResetEvent(false);
+            
 
-            var stateTimer = new Timer(commandChecker.CheckCommands,
-                                   autoEvent, 10000, 300000);
+            //Start the timer for checking commands
+            var stateTimer = new Timer(5000);
+            stateTimer.Elapsed += commandChecker.CheckCommands;
+            stateTimer.AutoReset = true;
+            stateTimer.Enabled = true;
+            
+            //Do we have a secret key
+            if (GetConfig("secret") == null || GetConfig("secret") == "")
+            {
+                LogError("It looks like this is a new installation. Specify your secret key by entering /buycraft secret [key]");
+            }
+            
         }
 
         public string GetDataFolder() => Path.Combine(new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)).LocalPath, "BuycraftNET");
@@ -53,14 +66,14 @@ namespace BuycraftNET
                 buycraftDataDirectory.Create();
             }
             
-            FileInfo buycraftConfigFile = new FileInfo(GetDataFolder() + "config.txt" );
+            FileInfo buycraftConfigFile = new FileInfo(GetDataFolder() + "/config.txt" );
             if (!buycraftConfigFile.Exists)
             {
                 FileStream fs = buycraftConfigFile.Create();
                 fs.Close();
             }
 
-            return GetDataFolder() + "config.txt";
+            return GetDataFolder() + "/config.txt";
         }
 
         public string GetConfig(string key)
@@ -74,9 +87,24 @@ namespace BuycraftNET
             this._parser.WriteFile(this.GetConfigFile(), this._config);
         }
 
-        public HttpClient GetHttpClient()
+        public void LogInfo(string message)
         {
-            return _httpClient;
+            Console.WriteLine("INFO [BuycraftNET] "+ message);
+        }
+
+        public void LogError(string message)
+        {
+            Console.WriteLine("ERROR [BuycraftNET] "+ message);
+        }
+
+        public ApiClient GetApiClient()
+        {
+            return _apiClient;
+        }
+
+        public PluginManager GetPluginManager()
+        {
+            return Context.PluginManager;
         }
         
     }
@@ -90,15 +118,11 @@ namespace BuycraftNET
             Plugin = plugin;
         }
 
-        public void CheckCommands(Object stateInfo)
+        public void CheckCommands(Object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("Config:" + Plugin.GetConfigFile());
-            Plugin.SetConfig("secret", "blah");
-            Console.WriteLine(Plugin.GetConfig("secret"));
-            Console.WriteLine("Do Command Check!");
-
-            var httpClient = Plugin.GetHttpClient();
-            
+//            var result = Plugin.GetApiClient().Get("https://plugin.buycraft.net/versions/sponge");
+//            Console.WriteLine(result.Result.ToString());
         }
+
     }
 }
--- BuycraftNET/BuycraftNET/Command/Buycraft.cs
+++ BuycraftNET/BuycraftNET/Command/Buycraft.cs
@@ -1,6 +1,18 @@
 using MiNET;
 using MiNET.Plugins.Attributes;
 using MiNET.Utils;
+using System;
+using System.Runtime.Remoting.Contexts;
+using MiNET.Net;
+using MiNET.Plugins;
+using MiNET.Worlds;
+using Newtonsoft.Json;
+
+using System.Reflection;
+
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
 
 namespace BuycraftNET.Command
 {
@@ -14,9 +26,109 @@ namespace BuycraftNET.Command
         }
 
         [Command(Name = "buycraft", Description = "Buycraft Management", Permission = "op")]
-        public void Execute(Player sender, string command, string param)
+        public void Execute(Player sender, string command, string param=null)
         {
+            Console.WriteLine(sender.PermissionLevel.ToString());
             sender.SendMessage(ChatColors.Green + "You asked for " + command + " " + param );
+            switch (command)
+            {
+                case "secret":
+                    Secret(sender, param);
+                    break;       
+                case "test":
+                    Test(sender);
+                    break;
+            }
         }
+
+        private void Secret(Player sender, string param)
+        {
+            Plugin.LogInfo("Checking Key");
+            Plugin.SetConfig("secret", param);
+            var storeInfo = Plugin.GetApiClient().GetInformation();
+
+            if (storeInfo.Result == null)
+            {
+                sender.SendMessage(ChatColors.Red + "This is not a valid secret key");                
+            }
+            else
+            {
+                sender.SendMessage(ChatColors.Green + "Secret key has been added!");
+                
+            }
+        }
+
+        private void Test(Player sender)
+        {
+
+            string command = "xp 100 Steve";
+            
+            var pm = Plugin.GetPluginManager();
+            var commands = pm.Commands;
+            
+            foreach (var pmCommand in commands)
+            {
+                if (command.StartsWith(pmCommand.Key))
+                {
+                    command = command.Replace(pmCommand.Key, "").Trim();
+                    var commandParams = command.Split(null);
+                    
+                    Console.WriteLine(commandParams.Length);
+                    
+                    Overload overload = pmCommand.Value.Versions.First().Overloads["default"];
+                    
+                    MethodInfo method = overload.Method;
+
+
+                    Console.WriteLine("method:" + method.ToString());
+                    Console.WriteLine("sender:" + sender.ToString());
+                    Console.WriteLine("CommandParmas::" + commandParams.ToString());
+                    
+                    Console.WriteLine("Invoke:");
+                    pm.GetType().GetTypeInfo().GetDeclaredMethod("ExecuteCommand").Invoke(pm, new object[] {method, sender, commandParams} );
+                    //execCommand.Invoke(pm, );
+                    /*
+                    dynamic commandInputJson = JsonConvert.DeserializeObject("{}");
+
+                    int x = 0;
+                    foreach (ParameterInfo parameter in method.GetParameters())
+                    {
+                        if (typeof (Player).IsAssignableFrom(parameter.ParameterType)) continue;
+
+                        if (commandParams.Length > x)
+                        {
+                            string argName = char.ToUpper(parameter.Name[0]) + parameter.Name.Substring(1);
+                            
+                            commandInputJson[argName] = commandParams[x];    
+                        }                        
+                        x++;
+                    }
+
+                    dynamic commandInput = JObject.FromObject(commandInputJson);
+                    
+                    Console.WriteLine(commandInput.ToString());
+                    
+                    
+                    
+                    JObject tobj = commandInput;
+                    if (commandInput != null)
+                    {
+                        if (tobj.Property("Time") != null)
+                        {
+                            Plugin.LogInfo($"Parameter: {commandInput["Time"].ToString()}");                            
+                        }                        
+                    }
+
+                    
+                    pm.HandleCommand(sender, pmCommand.Key, "default", tobj);
+                    */
+                    
+                    Console.WriteLine("Matching Command:" + pmCommand.Key);
+                    break;
+                }
+                
+            }                      
+        }
+        
     }
 }
